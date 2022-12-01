e8e1a28ac3a9ee4b6fcbedf5beb737a3b8fe1171
Hassan <hassansaleem194@gmail.com>
2022-12-01 10:34:49 +0000

    added google auth import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import gAuthPlugin from 'vue3-google-oauth2'
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
