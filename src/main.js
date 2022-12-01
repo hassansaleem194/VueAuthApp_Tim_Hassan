08c79e8d4190295b3e0571a16486fdd54b8a4d45
Tim Turvey <tturvey@hotmail.co.uk>
2022-12-01 11:26:30 +0000

    Completed tutorial - idpiframe_initialization_failed error

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import gAuthPlugin from 'vue3-google-oauth2'
-import './assets/main.css'
 
 const app = createApp(App)
+import gAuthPlugin from 'vue3-google-oauth2';
+let gauthClientId = '742461452195-2e8snplf9bk602lamp5bj360m5i1p0p9.apps.googleusercontent.com';
+app.use(gAuthPlugin, {
+  clientId: gauthClientId,
+  scope: 'email',
+  prompt: 'consent',
+})
 
 app.mount('#app')
